0b2cce0a7f2d1cf9aa9df1eb5e89962ba20fb428
Tian Liu <8xiaolong8@gmail.com>
2019-12-08 17:09:19 -0500

    128 changes

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -8,18 +8,18 @@ import LogoImg from "../../components/Header/LogoImg.js";
 import SectionCarousel from "./Sections/SectionCarousel.js";
 
 export default function Components(props) {
-  const { ...rest } = props;
-  return (
-    <div>
-      <Header
-        brand={<LogoImg/>}
-        rightLinks={<HeaderLinks />}
-        fixed
-        color="transparent"
-        {...rest}
-      >
-      </Header>
-      <SectionCarousel />
-    </div>
-  );
+  // const { ...rest } = props;
+  // return (
+  //   <div>
+  //     <Header
+  //       brand={<LogoImg/>}
+  //       rightLinks={<HeaderLinks />}
+  //       fixed
+  //       color="transparent"
+  //       {...rest}
+  //     >
+  //     </Header>
+  //     <SectionCarousel />
+  //   </div>
+  // );
 }
