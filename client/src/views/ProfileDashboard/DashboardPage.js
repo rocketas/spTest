90b694e0550aaa93b0de06cc0b08edf32988c1e0
Tian Liu <8xiaolong8@gmail.com>
2019-12-08 16:53:42 -0500

    128 changes

--- client/src/views/ProfileDashboard/DashboardPage.js
+++ client/src/views/ProfileDashboard/DashboardPage.js
@@ -1,43 +1,67 @@
+// import React from "react";
+// import { Container, Row, Col } from "shards-react";
+// import DefaultLayout from "../../layouts/Default.js";
+// import UserDetails from "../../components/user-profile-lite/UserDetails";
+// import UserAccountDetails from "../../components/user-profile-lite/UserAccountDetails";
+
+// import {connect} from 'react-redux'
+
+// function UserProfileLite(props){
+
+//   const checkIfLoggedIn = () => {
+//    if(props.isLoggedIn !== true){
+//       props.history.push("/login")
+//     }
+//   }
+
+  
+//   checkIfLoggedIn()
+
+//   return (
+//     <DefaultLayout>
+//     <Container fluid className="main-content-container px-4">
+//       <br></br>
+//       <Row>
+//         <Col lg="4">
+//           <UserDetails />
+//         </Col>
+//         <Col lg="8">
+//           <UserAccountDetails />
+//         </Col>
+//       </Row>
+//     </Container>
+//     </DefaultLayout>
+//   );
+// }
+// const mapStateToProps = (state) => {
+
+//   return({
+//     isLoggedIn: state.authentication.isLoggedIn,
+//     user: state.authentication.user
+//   })
+// }
+// export default connect(mapStateToProps)(UserProfileLite);
 import React from "react";
 import { Container, Row, Col } from "shards-react";
 import DefaultLayout from "../../layouts/Default.js";
+import PageTitle from "../../components/common/PageTitle";
 import UserDetails from "../../components/user-profile-lite/UserDetails";
 import UserAccountDetails from "../../components/user-profile-lite/UserAccountDetails";
 
-import {connect} from 'react-redux'
+const UserProfileLite = () => (
+  <DefaultLayout>
+  <Container fluid className="main-content-container px-4">
+    <br></br>
+    <Row>
+      <Col lg="4">
+        <UserDetails />
+      </Col>
+      <Col lg="8">
+        <UserAccountDetails />
+      </Col>
+    </Row>
+  </Container>
+  </DefaultLayout>
+);
 
-function UserProfileLite(props){
-
-  const checkIfLoggedIn = () => {
-   if(props.isLoggedIn !== true){
-      props.history.push("/login")
-    }
-  }
-
-  
-  checkIfLoggedIn()
-
-  return (
-    <DefaultLayout>
-    <Container fluid className="main-content-container px-4">
-      <br></br>
-      <Row>
-        <Col lg="4">
-          <UserDetails />
-        </Col>
-        <Col lg="8">
-          <UserAccountDetails />
-        </Col>
-      </Row>
-    </Container>
-    </DefaultLayout>
-  );
-}
-const mapStateToProps = (state) => {
-
-  return({
-    isLoggedIn: state.authentication.isLoggedIn,
-    user: state.authentication.user
-  })
-}
-export default connect(mapStateToProps)(UserProfileLite);
+export default UserProfileLite;
